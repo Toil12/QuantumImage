6e4e9c74d0f4f4c45399160211b0df3792649aef
Toil12 <femstls@outlook.com>
2023-06-27 03:09:02 +0800

    runnable FRQI structure

--- universal_test.py
+++ universal_test.py
@@ -1,3 +1,15 @@
 import numpy as np
-a=np.load(f"data/loss_epoch/22-163834.npy")
-print(a)
+from npy_append_array import NpyAppendArray
+file_name=f"./data/loss_epoch/test.npy"
+
+arr1 = np.array([[1, 2]])
+arr2 = np.array([[3, 4]])
+
+with NpyAppendArray(file_name) as npaa:
+    npaa.append(arr1)
+    npaa.append(arr2)
+    npaa.append(arr2)
+
+data = np.load(file_name, mmap_mode="r")
+
+print(data)
